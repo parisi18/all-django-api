2bd4ecef01cb881cb2b7a6575b4e1517355765ce
ParisiBR <parisi18@unifesp.br>
2024-06-18 17:20:39 -0300

    Matricula ViewSet

--- apps/escola/views.py
+++ apps/escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from apps.escola.models import Aluno, Curso
-from apps.escola.serializer import AlunoSerializer, CursoSerializer
+from apps.escola.models import Aluno, Curso, Matricula
+from apps.escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
 
 class AlunoViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos"""
@@ -11,3 +11,8 @@ class CursoViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """Exibindo todas as matrículas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
