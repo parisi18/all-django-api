6529b65311f3d716457c2fe5b7867445b971fdac
ParisiBR <parisi18@unifesp.br>
2024-06-18 16:42:20 -0300

    Aluno viewset

--- apps/escola/serializer.py
+++ apps/escola/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Aluno, Curso
+from apps.escola.models import Aluno, Curso
 
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
--- apps/escola/views.py
+++ apps/escola/views.py
@@ -1,9 +1,13 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from apps.escola.models import Aluno, Curso
+from apps.escola.serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {
-            'id': 1,
-            'nome': 'João',
-        }
-        return JsonResponse(aluno)
+class AlunoViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
