3addf1b3e2fd4e4b15780d11da6cfdf230785939
ParisiBR <parisi18@unifesp.br>
2024-06-18 17:15:08 -0300

    modelo Matricula

--- apps/escola/models.py
+++ apps/escola/models.py
@@ -21,4 +21,20 @@ class Curso(models.Model):
     nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False, default='B')
     
     def __str__(self):
-        return self.cod_curso
+        return self.descricao
+    
+class Matricula(models.Model):
+
+    PERIODO = (
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno'),
+    )
+
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    data_matricula = models.DateField()
+    periodo = models.CharField(max_length=1, choices=PERIODO, blank=False, null=False, default='M')
+    
+    def __str__(self):
+        return self.aluno.nome + ' - ' + self.curso.cod_curso
