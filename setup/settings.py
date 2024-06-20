fb5f8fa709fb309169e3eddac2fe46f31420e6e0
ParisiBR <parisi18@unifesp.br>
2024-06-20 13:36:46 -0300

    skgen config

--- setup/settings.py
+++ setup/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2gr77h&cnn9wmzw=fmve&+zlzd25y0@fyi5y00smq&dqtt@z&z'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +45,7 @@ INSTALLED_APPS = [
 
     # apps
     'apps.escola.apps.EscolaConfig',
+    'apps.clientes.apps.ClientesConfig',
 
     # django rest framework
     'rest_framework',
