47f7f4d3b885bac4760a24e90bd34d9879ed2477
ParisiBR <parisi18@unifesp.br>
2024-06-21 13:01:33 -0300

    validação rg e celular

--- apps/clientes/serializers.py
+++ apps/clientes/serializers.py
@@ -16,3 +16,13 @@ class ClienteSerializer(serializers.ModelSerializer):
         if not nome.isalpha():
             raise serializers.ValidationError("O nome deve conter apenas letras.")
         return nome
+    
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("O RG deve conter 9 dígitos.")
+        return rg
+    
+    def validate_celular(self, celular):
+        if len(celular) < 14:
+            raise serializers.ValidationError("O celular deve conter no mínimo 11 dígitos.")
+        return celular
