27e44b4e4c4dca35b8051ca5bc026edf65e3e027
ParisiBR <parisi18@unifesp.br>
2024-06-21 15:57:28 -0300

    DRF paginação

--- setup/settings.py
+++ setup/settings.py
@@ -133,3 +133,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django Rest Framework Pagination
+# https://www.django-rest-framework.org/api-guide/pagination/
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
